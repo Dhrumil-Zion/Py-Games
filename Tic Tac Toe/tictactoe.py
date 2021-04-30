a4be7f4b41c54462948459558bfb31f433022020
Dhrumil <dhrumilg699@gmail.com>
2021-04-30 17:50:55 +0530

    Tic Tac Toe in cmd comleted

--- Tic Tac Toe/tictactoe.py
+++ Tic Tac Toe/tictactoe.py
@@ -1,23 +1,23 @@
-board = [' ' for c in range(10)]
+board = [' ' for x in range(10)]
 
-def insertLetter(letter,position):
-    board[position] = letter
+def insertLetter(letter,pos):
+    board[pos] = letter
 
-def sapceIsFree(position):
-    return board[position] == ''
+def spaceIsFree(pos):
+    return board[pos] == ' '
 
 def printBoard(board):
-    print("   |   |   ")
-    print(" "+board[1]+" | "+board[2]+" | "+" "+board[3])
-    print("   |   |   ")
-    print("-----------")
-    print("   |   |   ")
-    print(" "+board[4]+" | "+board[5]+" | "+" "+board[6])
-    print("   |   |   ")
-    print("-----------")
-    print("   |   |   ")
-    print(" "+board[7]+" | "+board[8]+" | "+" "+board[9])
-    print("   |   |   ")
+    print('   |   |   ')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |   ')
+    print('------------')
+    print('   |   |   ')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |   ')
+    print('------------')
+    print('   |   |   ')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |   ')
 
 def isBoardFull(board):  # sourcery skip: return-identity
     if board.count(' ') > 1:
@@ -25,23 +25,102 @@ def isBoardFull(board):  # sourcery skip: return-identity
     else:
         return True
 
-def winner(board):
-    if board[1] == board[2] == board[3]:
-        print("{} wins ".format(board[1]))
-    elif board[4] == board[5] == board[6]:
-        print("{} wins ".format(board[4]))
-    elif board[7] == board[8] == board[9]:
-        print("{} wins ".format(board[7]))
+def IsWinner(b,l):
+    return ((b[1] == l and b[2] == l and b[3] == l) or
+    (b[4] == l and b[5] == l and b[6] == l) or
+    (b[7] == l and b[8] == l and b[9] == l) or
+    (b[1] == l and b[4] == l and b[7] == l) or
+    (b[2] == l and b[5] == l and b[8] == l) or
+    (b[3] == l and b[6] == l and b[9] == l) or
+    (b[1] == l and b[5] == l and b[9] == l) or
+    (b[3] == l and b[5] == l and b[7] == l))
 
-    elif board[1] == board[4] == board[7]:
-        print("{} wins ".format(board[1]))
-    elif board[2] == board[5] == board[8]:
-        print("{} wins ".format(board[1]))
-    elif board[3] == board[6] == board[9]:
-        print("{} wins ".format(board[1]))
-    
-    elif board[1] == board[5] == board[9]:
-        print("{} wins ".format(board[1]))
-    elif board[3] == board[5] == board[7]:
-        print("{} wins ".format(board[3]))
+def playerMove():
+    run = True
+    while run:
+        move = input("please select a position to enter the X between 1 to 9")
+        try:
+            move = int(move)
+            if move > 0 and move < 10:
+                if spaceIsFree(move):
+                    run = False
+                    insertLetter('X' , move)
+                else:
+                    print('Sorry, this space is occupied')
+            else:
+                print('please type a number between 1 and 9')
 
+        except:
+            print('Please type a number')
+
+def computerMove():
+    possibleMoves = [x for x , letter in enumerate(board) if letter == ' ' and x != 0  ]
+    move = 0
+
+    for let in ['O' , 'X']:
+        for i in possibleMoves:
+            boardcopy = board[:]
+            boardcopy[i] = let
+            if IsWinner(boardcopy, let):
+                move = i
+                return move
+
+    cornersOpen = [i for i in possibleMoves if i in [1 , 3 , 7 , 9]]
+    if cornersOpen:
+        move = selectRandom(cornersOpen)
+        return move
+
+    if 5 in possibleMoves:
+        move = 5
+        return move
+
+    edgesOpen = [i for i in possibleMoves if i in [2,4,6,8]]
+    if edgesOpen:
+        move = selectRandom(edgesOpen)
+        return move
+
+def selectRandom(li):
+    import random
+    ln = len(li)
+    r = random.randrange(0,ln)
+    return li[r]
+
+def main():
+    print("Welcome to the game!")
+    printBoard(board)
+
+    while not(isBoardFull(board)):
+        if not(IsWinner(board , 'O')):
+            playerMove()
+            printBoard(board)
+        else:
+            print("sorry you loose!")
+            break
+
+        if not(IsWinner(board , 'X')):
+            move = computerMove()
+            if move == 0:
+                print(" ")
+            else:
+                insertLetter('O' , move)
+                print('computer placed an o on position' , move , ':')
+                printBoard(board)
+        else:
+            print("you win!")
+            break
+
+
+
+
+    if isBoardFull(board):
+        print("Tie game")
+
+# sourcery skip: remove-unnecessary-else, swap-if-else-branches
+while True:
+    x = input("Do you want to play again? (y/n)")
+    if x.lower() == 'y':
+        board = [' ' for x in range(10)]
+        print('--------------------')
+        main()
+    else:
+        break
