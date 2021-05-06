89dfcd439d38dec87c99d04c0a1d6b97e09cbaf1
Dhrumil <dhrumilg699@gmail.com>
2021-05-07 00:02:08 +0530

    Completion of MatchMaker Game

--- Match Maker/main_game_file.py
+++ Match Maker/main_game_file.py
@@ -1,50 +1,47 @@
 import random
-import time 
-from tkinter import Tk,Button,DISABLED
+import time
+from tkinter import Tk , Button , DISABLED
 
-
-def show_symbol(v,g):
+def show_symbol(x,y):
     global first
-    global previousx,previousy
-    buttons[v,g]['text'] = button_symbol[v,g]
-    buttons[v,g].update_idletasks()
+    global previousx , previousy
+    buttons[x,y]['text'] = button_symbols[x,y]
+    buttons[x,y].update_idletasks()
 
     if first:
-        previousx = v
-        previousy = g
+        previousx = x
+        previousy = y
         first = False
-    elif previousx!= v or previousy!=g:
-        if buttons[previousx,previousy]['text'] != buttons[v,g]['text']:
+    elif previousx != x or previousy != y:
+        if buttons[previousx,previousy]['text'] != buttons[x,y]['text']:
             time.sleep(0.5)
             buttons[previousx,previousy]['text'] = ' '
-            buttons[v,g]['text'] = ' '
+            buttons[x,y]['text'] = ' '
         else:
             buttons[previousx,previousy]['command'] = DISABLED
-            buttons[v,g]['command'] = DISABLED
+            buttons[x,y]['command'] = DISABLED
         first = True
 
-
-win =Tk()
-win.title("Match Maker")
-win.resizable(width=False,height=False)
+win = Tk()
+win.title('Matchmaker')
+win.resizable(width=False , height=False)
 first = True
 previousx = 0
 previousy = 0
-
-buttons ={}
-button_symbol = {}
-symbols =[u'\u2702',u'\u2705',u'\u2708',u'\u270A',u'\u270B',u'\u270C',u'\u270F',
-        u'\u2712',u'\u2714',u'\u2716',u'\u2728',
-        u'\u2702',u'\u2705',u'\u2708',u'\u270A',u'\u270B',u'\u270C',u'\u270F',
-        u'\u2712',u'\u2714',u'\u2716',u'\u2728']
+buttons = { }
+button_symbols = { }
+symbols = [u'\u2702',u'\u2705',u'\u2708',u'\u2709',u'\u270A',u'\u270B',
+            u'\u270C',u'\u270F',u'\u2712',u'\u2714',u'\u2716',u'\u2728',
+            u'\u2702',u'\u2705',u'\u2708',u'\u2709',u'\u270A',u'\u270B',
+            u'\u270C',u'\u270F',u'\u2712',u'\u2714',u'\u2716',u'\u2728']
 
 random.shuffle(symbols)
 
-for v in range(6):
-    for g in range(4):
-        button = Button(command=show_symbol(v,g), width=6,height=6)
-        button.grid(column=v,row=g)
-        buttons[v,g] = button
-        button_symbol[v,g] = symbols.pop()
+for x in range(6):
+    for y in range(4):
+        button = Button(command = lambda x=x , y=y: show_symbol(x,y) , width = 10, height = 8)
+        button.grid(column = x , row = y)
+        buttons[x,y] = button
+        button_symbols[x,y] = symbols.pop()
 
 win.mainloop()
